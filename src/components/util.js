33c233a7180229abcef74cc4b8303e4a252b296f
Hector A. Acosta <hector.acosta.pozo@gmail.com>
2020-01-11 16:11:31 -0400

    Moved to components

--- src/components/util.js
+++ src/components/util.js
@@ -3,20 +3,11 @@ import styled from 'styled-components'
 import logo from '../images/dark.svg'
 
 export const Logo = styled.div`
-width: 100%;
-background-image: url(${logo});
-background-repeat: no-repeat;
-background-position: center;
-background-size: 60%;
-height: 100px;
+
 `;
 
 export const LoginView = styled.div`
-min-height: 300px;
-padding: 20px;
-position: relative;
-max-width: 375px;
-margin: 0 auto;
+
 
 @media only screen and (min-width: 425px) {}
 @media only screen and (min-width: 768px) {}
--- src/pages/login.js
+++ src/pages/login.js
@@ -10,8 +10,21 @@ import { Logo, LoginView } from '../components/util'
 const LoginPage = () => (
   <Layout>
     <SEO title="Login" description="Provisioning site" />
-    <LoginView>
-      <Logo src={logo} />
+    <LoginView style={{
+      minHeight: '300px',
+      padding: '20px',
+      position: 'relative',
+      maxWidth: '375px',
+      margin: '0 auto'
+    }}>
+      <div style={{
+        width: '100%',
+        backgroundImage: `url(${logo})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '60%',
+        height: '100px'
+      }} src={logo} />
       <LoginForm />
     </LoginView>
   </Layout>
